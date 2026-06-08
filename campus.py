23ed70842147dd9bc97c5fec36ef26a2d356b863
ranaeldaly03-ctrl <ranaeldaly03@gmail.com>
2026-06-08 19:23:34 +0300

    Fix socketio render

--- campus.py
+++ campus.py
@@ -4916,6 +4916,12 @@ if __name__ == '__main__':
     # app.run(debug=True)
     # app.run(host="0.0.0.0", port=5000)
     # socketio.run(app, debug=True)
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+        allow_unsafe_werkzeug=True
+    )
     app.run(host="0.0.0.0", port=5000)
 
