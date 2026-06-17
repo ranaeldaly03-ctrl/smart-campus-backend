a30b0cb2544896cf1633a043b5e65ae99250502f
ranaeldaly03-ctrl <ranaeldaly03@gmail.com>
2026-06-17 06:12:28 +0300

    Add quiz editor page

--- campus.py
+++ campus.py
@@ -5759,6 +5759,11 @@ def register_card_page():
 def gate_logs_page():
     return render_template("gate_logs.html")
 
+
+@app.route("/Prof_quiz_editor")
+def prof_quiz_editor():
+    return render_template("Prof_quiz_editor.html")    
+
           
 
 
